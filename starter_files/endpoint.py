5c5a8618e97b9560ba830cb3ab9576d26dfdf412
HajimeK <smec.kawata@gmail.com>
2021-07-12 07:14:55 -0700

    WIP : trouble in endpoint.py

--- starter_files/endpoint.py
+++ starter_files/endpoint.py
@@ -3,9 +3,10 @@ import json
 
 # URL for the web service, should be similar to:
 # 'http://8530a665-66f3-49c8-a953-b82a2d312917.eastus.azurecontainer.io/score'
-scoring_uri = ''
+scoring_uri = 'http://d8e9f6ad-4112-4417-97c0-01b4246b284a.japaneast.azurecontainer.io/score'
 # If the service is authenticated, set the key or token
-key = ''
+key = str(open('./starter_files/key.txt', 'r').readline().split(':')[1])
+#key ='sYDHOfPPfLTb0w5gDucnNQfT8VinfhBf'
 
 # Two sets of data to score, so we get two results back
 data = {"data":
---
+++ starter_files/work/work.md
@@ -0,0 +1,24 @@
+
+service principle created
+
+![](2021-07-11-00-02-56.png)
+
+![](2021-07-11-00-03-32.png)
+
+
+az ml workspace share worked
+![](2021-07-11-00-00-29.png)
+
+
+App Insights
+
+
+![](2021-07-12-01-37-47.png)
+
+Swagger is also there
+![](2021-07-12-02-02-50.png)
+
+
+![](2021-07-12-01-38-49.png)
+
+![](2021-07-12-02-00-29.png)
